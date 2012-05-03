8abff53296a0b7f3f37cd800573af8a5f05bb6d8
M K <miro.klenk@informatik.hs-fulda.de>
2012-05-04 00:37:02 +0200

    changed weather API to google

--- src/mobcom/app/Mobcom_appActivity.java
+++ src/mobcom/app/Mobcom_appActivity.java
@@ -2,14 +2,24 @@ package mobcom.app;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.security.Timestamp;
+import java.util.Date;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Mobcom_appActivity extends Activity 
-{
+{	
+	private TextView tmr;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) 
@@ -30,44 +40,59 @@ public class Mobcom_appActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        ParseW pw = new ParseW();
-        
         TextView date = (TextView)findViewById(R.id.date);        
         TextView m1 = (TextView)findViewById(R.id.morningToday);         
         TextView n1 = (TextView)findViewById(R.id.noonToday);         
         TextView e1 = (TextView)findViewById(R.id.eveToday);
         
-        TextView tmr = (TextView)findViewById(R.id.tomorrow);
+        /*tmr = (TextView)findViewById(R.id.tomorrow);
         
         TextView m2 = (TextView)findViewById(R.id.mT);         
         TextView n2 = (TextView)findViewById(R.id.nT);         
-        TextView e2 = (TextView)findViewById(R.id.eT);
+        TextView e2 = (TextView)findViewById(R.id.eT);*/        
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() 
+        {        	
+            public void onLocationChanged(Location location) 
+            {            	
+            	// Called when a new location is found by the network location provider.
+            	//makeUseOfNewLocation(location);
+            	Toast.makeText(Mobcom_appActivity.this, "foobar", Toast.LENGTH_LONG).show();
+            	tmr.setText("asd");
+            }
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+            public void onProviderEnabled(String provider) {}
+            public void onProviderDisabled(String provider) {}
+        };
+        // Register the listener with the Location Manager to receive location updates
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
         
-        String t;
+        /* weather related stuff starts here */
+        ParseW pw = new ParseW();                        
         try 
-		{        	
-			BufferedReader weather = pw.read("http://www.wetter.de/wettervorhersage/49-3240-50/wetter-fulda.html");
+		{        				
 			BufferedReader loc = pw.read("http://www.infosniper.net");
-			String line = loc.readLine();
-			
+			String line = loc.readLine();			
 			while(line != null)
 			{
-				t = pw.search("content-td2\">(.+?)<", line);
-				if(!t.equals("nomatch"))
-				{
-					pw.setLoc(t);
-				}
+				pw.search("content-td2\">(.+?)<", line, true);				
 				line = loc.readLine();
-			}			
+			}		
+			String url = "http://www.google.com/ig/api?weather=" + pw.getLocAt(1);			
+			//Toast.makeText(Mobcom_appActivity.this, url, Toast.LENGTH_LONG).show();
+			BufferedReader weather = pw.read(url);
 			line = weather.readLine();
 			while(line != null)
 			{
-				t = pw.search("temperature\">(\\d+?)&nbsp;&deg;C<", line);
-				if(!t.equals("nomatch"))
-				{
-					pw.setData(t);
-				}
-				//pw.setTemp());
+				// search for min temperature, this will be at index 0
+				pw.search("<low data=\"(\\d+?)\"/>", line, false);				
+				// search for max temperature, this will be at index 1
+				pw.search("<high data=\"(\\d+?)\"/>", line, false);							
+				// search for condition, this will be at index 2
+				pw.search("<condition data=\"(.+?)\"/>", line, false);				
 				line = weather.readLine();
 			}
 		 } 
@@ -80,18 +105,16 @@ public class Mobcom_appActivity extends Activity
 		 {
 			 e.printStackTrace();
 		 }
-		 //pw.moveTempToData();
-		 date.setText("Wetter in "+ pw.getLocAt(1) + " " + pw.getDate());
+		 date.setText("Wetter in " + pw.getLocAt(1) + pw.getDate());		 
+		 m1.setText("min. " + pw.convert(Integer.parseInt(pw.getDataAt(0))) + "°C | "); // min
+		 n1.setText("max. " + pw.convert(Integer.parseInt(pw.getDataAt(1))) + "°C"); // max
+		 e1.setText("Wetterlage: " + pw.getCond(pw.getDataAt(2))); // condition
 		 
-		 m1.setText("Morgens: " + pw.getDataAt(1) + "°C");		 
-		 n1.setText("Mittags: " + pw.getDataAt(2) + "°C");
-		 e1.setText("Abends: " + pw.getDataAt(3) + "°C");
+		 //pw.nextDate();
+		 //tmr.setText("");
 		 
-		 pw.nextDate();
-		 tmr.setText("Wetter in "+ pw.getLocAt(1) + " " + pw.getDate());
-		 
-		 m2.setText("Morgens: " + pw.getDataAt(5) + "°C");		 
-		 n2.setText("Mittags: " + pw.getDataAt(6) + "°C");
-		 e2.setText("Abends: " + pw.getDataAt(7) + "°C");
-    }
+		 /*m2.setText("");		 
+		 n2.setText("");
+		 e2.setText("");*/
+    }        
 }
--- src/mobcom/app/ParseW.java
+++ src/mobcom/app/ParseW.java
@@ -7,6 +7,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,11 +17,36 @@ public class ParseW
 	private String day;
 	private ArrayList<String> data;
 	private ArrayList<String> locD;
+	private HashMap<String, String> cond; // holds the different weather conditions and their respective translation
 	
 	public ParseW()
 	{
 		locD = new ArrayList<String>();
-		data = new ArrayList<String>();		
+		data = new ArrayList<String>();
+		cond = new HashMap<String, String>();
+		cond.put("Chance of Rain", "Vereinzelt Regen");
+		cond.put("Sunny", "Sonnig");
+		cond.put("Mostly Sunny", "Meistens Sonnig");
+		cond.put("Partly Cloudy", "Vereinzelt Bewölkt");
+		cond.put("Mostly Cloudy", "Meistens Bewölkt");
+		cond.put("Chance of Storm", "Vereinzelt Stürmisch");
+		cond.put("Showers", "Wolkenbruch");
+		cond.put("Rain", "Regen");
+		cond.put("Chance of Snow", "Vereinzelt Schnee");
+		cond.put("Cloudy", "Bewölkt");
+		cond.put("Mist", "Nebel");
+		cond.put("Storm", "Stürmisch");
+		cond.put("Thunderstorm", "Gewitter");
+		cond.put("Chance of TStorm", "Vereinzelt Gewitter");
+		cond.put("Sleet", "Graupel");
+		cond.put("Snow", "Schnee");
+		cond.put("Icy", "Eisig");
+		cond.put("Dust", "Staubig");
+		cond.put("Fog", "Starker Nebel");
+		cond.put("Smoke", "Dunstig");
+		cond.put("Haze", "Start Dunstig");
+		cond.put("Flurries", "Starker Schneefall");		
+		cond.put("Clear", "Klar");
 		setDate();
 	}
 	
@@ -44,8 +70,9 @@ public class ParseW
 	{
 		return this.date;
 	}
-
-	public String search(String pattern, String text)
+	
+	/* loc determines if the result is written to locD/data */
+	public void search(String pattern, String text, boolean loc)
 	{				
 		// Create a Pattern object
 	  	Pattern r = Pattern.compile(pattern, Pattern.DOTALL);
@@ -54,10 +81,30 @@ public class ParseW
 	  	Matcher m = r.matcher(text);
 	  	if(m.find())
 	  	{	  		
-	  		return m.group(1);  
+	  		//return m.group(1);
+	  		if(loc)
+	  		{
+	  			setLoc(m.group(1));
+	  		}	  		
+	  		else
+	  		{
+	  			setData(m.group(1));
+	  		}
 	  	}
-	  	return "nomatch";
-	}			
+	  	//return "nomatch";
+	}	
+	
+	public String convert(float f)
+	{
+		//(°F  -  32)  x  5/9 = °C
+		int t = (int)(f - 32) * 5/9;
+		return Integer.toString(t);
+	}
+	
+	public String getCond(String input)
+	{
+		return cond.get(input);
+	}
 	
 	public void setData(String value)
 	{
